158d2052cf2d755eb5f7c81d96027dbd541d9855
semayagi <yagid.semen07.07@yandex.ru>
2026-03-24 21:11:49 +0300

    feat(singup page): Password input now hides the password

--- src/ui/inputs/AuthInput/AuthInput.tsx
+++ src/ui/inputs/AuthInput/AuthInput.tsx
@@ -1,20 +1,37 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import styles from './AuthInput.module.css'
+import { useState } from "react";
+import styles from "./AuthInput.module.css";
+
+interface AuthInputProps {
+  type?: "password" | "email" | "text";
+  id: string;
+  placeholder: string;
+  required?: boolean;
+}
 
 /* This element changes its background-color depending on whether the input field is empty or not */
-export default function AuthInput({id, placeholder, required}: {id: string, placeholder: string, required?: boolean}) {
-    const [className, setClassName] = useState(styles.empty)
+export default function AuthInput({
+  type,
+  id,
+  placeholder,
+  required,
+}: AuthInputProps) {
+  const [className, setClassName] = useState(styles.empty);
 
-    function handleInput(target: HTMLInputElement) {
-        if (target.value)
-            setClassName("")
-        else
-            setClassName(styles.empty)
-    }
+  function handleInput(target: HTMLInputElement) {
+    if (target.value) setClassName("");
+    else setClassName(styles.empty);
+  }
 
-    return (
-        <input className={`basic-input ${styles.input} ${className}`} onChange={(e) => handleInput(e.target)} type="text" id={id} placeholder={placeholder} required={required ?? true}/>
-    )
+  return (
+    <input
+      className={`basic-input ${styles.input} ${className}`}
+      onChange={(e) => handleInput(e.target)}
+      type={type ?? "text"}
+      id={id}
+      placeholder={placeholder}
+      required={required ?? true}
+    />
+  );
 }
--- src/ui/inputs/LabelledAuthInput/LabelledAuthInput.tsx
+++ src/ui/inputs/LabelledAuthInput/LabelledAuthInput.tsx
@@ -1,17 +1,38 @@
-import styles from './LabelledAuthInput.module.css'
-import AuthInput from '../AuthInput/AuthInput'
+import styles from "./LabelledAuthInput.module.css";
+import AuthInput from "../AuthInput/AuthInput";
+import AuthInputProps from "../AuthInput/AuthInput";
 
-export default function LabelledAuthInput({label, placeholder, required}: {label: string, placeholder: string, required?: boolean}) {
-    let visibility: "visible" | "hidden" = "visible";
-    if (required != null && required == false) {
-        visibility = "hidden";
-    }
-    return (
-        <div className={styles.container}>
-            <label htmlFor={label}>
-                <span className={styles.text}>{label} <span style={{ visibility: visibility, color: "red" }}>*</span></span>
-                <AuthInput id={label} placeholder={placeholder} required={required ?? true}></AuthInput>
-            </label>
-        </div>
-    )
+interface LabelledAuthInputProps {
+  type?: "password" | "email" | "text";
+  label: string;
+  placeholder: string;
+  required?: boolean;
+}
+
+export default function LabelledAuthInput({
+  type,
+  label,
+  placeholder,
+  required,
+}: LabelledAuthInputProps) {
+  let visibility: "visible" | "hidden" = "visible";
+  if (required != null && required == false) {
+    visibility = "hidden";
+  }
+  return (
+    <div className={styles.container}>
+      <label htmlFor={label}>
+        <span className={styles.text}>
+          {label}{" "}
+          <span style={{ visibility: visibility, color: "red" }}>*</span>
+        </span>
+        <AuthInput
+          type={type}
+          id={label}
+          placeholder={placeholder}
+          required={required}
+        ></AuthInput>
+      </label>
+    </div>
+  );
 }
